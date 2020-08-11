3958601e97dc637aa18805a63c7fb470828e1ffe
Kik Engineering <engineering@kik.com>
2020-08-11 15:35:44 -0400

    all: add license, add details to python package
    GitOrigin-RevId: 88aedf9c0841fdc5a61f773b41e815feb8456c72

--- python/setup.py
+++ python/setup.py
@@ -3,11 +3,24 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='agoraapi',
-    version='0.17.0',
-    packages=find_packages(include=["agoraapi", "agoraapi.*"]),
-    author='kin',
+    name='agora-api',
+    version='0.19.0',
+    description='Agora API for Python',
+    author='Kik Engineering',
+    author_email='engineering@kik.com',
+    url='https://github.com/kinecosystem/agora-api',
+    license='MIT',
+    packages=find_packages(include=["agora", "agora.*"]),
+    long_description=open("../README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
     install_requires=[
         "protobuf"
-    ]
+    ],
 )
