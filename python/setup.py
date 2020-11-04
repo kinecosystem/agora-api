370f6276cde57fef5a9a9ce94588d18d4be6e694
Kik Engineering <engineering@kik.com>
2020-11-04 15:45:17 -0500

    all: release v0.21.0
    GitOrigin-RevId: b6f5ded603f279276df03dbc47091a2fc45b9671

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='agora-api',
-    version='0.20.0',
+    version='0.21.0',
     description='Agora API for Python',
     author='Kik Engineering',
     author_email='engineering@kik.com',
